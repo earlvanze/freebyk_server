5448f7bb8ca263a599de887406ec51ac0b3b44a6
Free Byk <freebyk@ps432709.dreamhostps.com>
2015-08-30 17:25:15 -0700

    Created nearby route Implemented naive selection algorithm

--- common/models/station.js
+++ common/models/station.js
@@ -1,3 +1,30 @@
 module.exports = function(Station) {
+    var selectionAlgorithm = function(location, stations){
+	return stations;
+    };
 
+    
+    Station.nearby = function(location, distance, callback){
+	Station.find({
+	    where: {
+		geolocation: {
+		    near: location,
+		    maxDistance: distance}
+	    }
+	},
+		     function(error, stations){
+			 if(error){
+			     return error;
+			 }
+			 callback(null, selectionAlgorithm(location, stations));
+		     });
+    };
+    Station.remoteMethod(
+	"nearby",
+	{
+	    http: {path: "/nearby", verb: "get"},
+	    accepts: [{arg: "location", type: "geopoint"},
+		      {arg: "distance", type: "number"}],
+	    returns: {arg: "stations", type: "array"}
+	});
 };
